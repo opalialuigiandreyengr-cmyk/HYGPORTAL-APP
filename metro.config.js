c1514ec9726af20096220ffa355b0dc69109d751
Ryugi <opalialuigiandrey.engr@gmail.com>
2026-08-14 14:48:51 +0800

    v1.5.6: Added Metro blockList and package overrides to fix codegen error on EAS Build

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,9 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('wasm');
 
+// Prevent Metro from processing nested duplicate react-native modules during eager bundling
+config.resolver.blockList = [
+  /node_modules\/.*\/node_modules\/react-native\/.*/,
+];
+
 module.exports = config;
